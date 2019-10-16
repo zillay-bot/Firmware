04a785b1e11d66bc8999e92be489f63948bcd855
Beat Küng <beat-kueng@gmx.net>
2019-10-16 15:57:53 +0200

    WorkQueueManager: increase stack size of I2C wq's by 10 bytes
    Was low by 8 bytes on fmu-v5.

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -57,10 +57,10 @@ static constexpr wq_config_t SPI4{"wq:SPI4", 1400, -4};
 static constexpr wq_config_t SPI5{"wq:SPI5", 1400, -5};
 static constexpr wq_config_t SPI6{"wq:SPI6", 1400, -6};
 
-static constexpr wq_config_t I2C1{"wq:I2C1", 1250, -7};
-static constexpr wq_config_t I2C2{"wq:I2C2", 1250, -8};
-static constexpr wq_config_t I2C3{"wq:I2C3", 1250, -9};
-static constexpr wq_config_t I2C4{"wq:I2C4", 1250, -10};
+static constexpr wq_config_t I2C1{"wq:I2C1", 1260, -7};
+static constexpr wq_config_t I2C2{"wq:I2C2", 1260, -8};
+static constexpr wq_config_t I2C3{"wq:I2C3", 1260, -9};
+static constexpr wq_config_t I2C4{"wq:I2C4", 1260, -10};
 
 static constexpr wq_config_t att_pos_ctrl{"wq:att_pos_ctrl", 6600, -11}; // PX4 att/pos controllers, highest priority after sensors
 
