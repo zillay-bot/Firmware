d35df741b6df233c24e9fee434421e6c6dee6432
Lorenz Meier <lorenz@px4.io>
2020-01-25 16:05:08 +0100

    [Auterion] Model downloader: Load less This focuses the download on the Allmend 1 model, which loads faster.

--- custom/model_downloader.bash
+++ custom/model_downloader.bash
@@ -15,12 +15,12 @@ BUILD_DIR=$2
 
 # Check if model files are present
 if test -d "$SRC_DIR/Tools/sitl_gazebo/models/almend1"; then
-    echo "Auterion Simulation Models found, continuing.."
+    echo "Auterion Allmend Simulation Models found, continuing.."
 else
     # Not found, download and unzip
-    echo "Auto-Downloading Auterion Simulation Models.."
+    echo "Auto-Downloading Auterion Allmend Simulation Models.."
     cd $SRC_DIR/Tools/sitl_gazebo/models
-    wget https://au-models.s3.amazonaws.com/auterion-models.zip
-    unzip auterion-models.zip
-    rm -f auterion-models.zip
+    wget https://au-models.s3.amazonaws.com/almend1.zip
+    unzip almend1.zip
+    rm -f almend1.zip
 fi
